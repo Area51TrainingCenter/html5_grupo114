9eecacb169496d4b31b6764c524b40a920dfa4cf
alumno <alumno@Area51s-MacBook-Proo.local>
2019-05-25 12:48:14 -0500

    update

---
+++ Modulo3/intro js/js/main.js
@@ -0,0 +1,142 @@
+// var  dar inicio a la creación de una variable 
+
+var dato;
+var nombre_persona;
+var APELLIDO_PERSONA;
+var edadPersona;
+
+var nombre_paterno;
+
+var nombrePaterno;
+
+var JuanCarlos;
+
+// revisar link http://codigolinea.com/2008/05/25/estilo-de-programacion-y-convencion-de-nombres-ii/
+// no se puede escribir de esta manera
+/*
+var 1data;
+var dato nombre;
+var 1;
+*/
+
+dato="Juan carlos";
+nombre_persona=dato;
+
+console.log("Escribiendo desde JS");
+console.log("Nuevo texto");
+console.log("dato");
+console.log(dato);
+
+
+
+var nombre;
+var correo;
+var edad;
+var telefono;
+
+nombre="JC";
+correo="jrt";
+edad="22";
+telefono="989495566";
+
+
+console.log("nombre");
+console.log(nombre);
+console.log(correo);
+console.log(edad);
+console.log(telefono);
+
+
+var curso="HTML"
+var CURSO="JS"
+
+console.log(curso);
+console.log(CURSO);
+
+
+
+nombre="JC";
+correo="jrt";
+edad="22";
+telefono="989495566";
+sueldo=100;
+sueldo="100";
+var num1=20;
+var num2=30;
+var numero1;
+numero1=40;
+
+
+var n1=20;
+var n2=40;
+
+// suma +
+// resta - 
+// division /
+// multiplicacion *
+// division modular %
+
+var resta=n1-n2;
+
+console.log(n1+n2);
+console.log(n1-n2);
+console.log(n1*n2);
+console.log(n1/n2);
+console.log(n1%n2);
+
+
+console.log(n1-n2);
+
+
+
+console.log(n1-n2);
+
+
+console.log(resta);
+console.log(resta);
+console.log(resta);
+
+
+var dato;
+
+
+
+var dato="text"; // variable en varibale cuando se asigna el valor
+
+dato=12; // variable numerica 
+
+var dato_altas=dato.toUpperCase();
+
+var estado=dato.indexOf("t");
+console.log(dato_altas);
+console.log(estado);
+
+
+var nombre="   juan    Carlos   ";
+
+nombre.trim();  // "juan    Carlos"
+
+console.log(nombre.trim());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Modulo3/variables/main.js
@@ -0,0 +1,36 @@
+//
+
+var num1=10;
+var num2=30;
+
+
+var n1="20";
+var n2="30";
+
+
+
+var suma= n1+n2;
+var suma="20"+"30" //2030
+
+
+var nombre="JC"
+var apellido="RT"
+var nombrecompleto=nombre+" "+apellido // JCRT
+var nombrecompleto="JC"+ " "+"RT"  // JC RT
+
+
+
+var suma=n1+n2;
+var n1="20";
+var n2="10";
+
+var resta=n1-n2;
+console.log(resta);
+
+
+var n1="30";
+console.log(n1);
+//var cambio=n1-0;
+var cambio=parseInt(n1);
+console.log(cambio);
+
